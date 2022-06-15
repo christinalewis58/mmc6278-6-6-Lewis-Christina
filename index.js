0e692a83cc032c035540812976815596767e2f3d
Christina Lewis <clewis13@ufl.edu>
2022-06-15 18:18:57 -0400

    initial commit, moved all images into img folder

---
+++ index.js
@@ -0,0 +1,5 @@
+//When the `button.hamburger-btn` element is clicked on smaller screens, the hamburger menu should open/close. Again, toggling the `show-menu` class on the `.hamburger-menu` element should show/hide the menu.
+//Clicking outside of `.hamburger-menu` should close the menu if it is open.
+//Clicking inside of `.hamburger-menu` should NOT close the menu.
+//Pressing the Escape key when the menu is open should close the menu and focus `button.hamburger-btn`.
+//Under all circumstances, when the menu is open, `aria-expanded` should be set to `true` on `button.hamburger-btn`, and set to `false` when the menu is closed.
