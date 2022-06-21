22cad68296871f44a98dc362979a63742eb83d79
Christina Lewis <clewis13@ufl.edu>
2022-06-21 10:20:09 -0400

    coded close menu on escape

--- index.js
+++ index.js
@@ -1,15 +1,22 @@
 //When the `button.hamburger-btn` element is clicked on smaller screens, the hamburger menu should open/close. Again, toggling the `show-menu` class on the `.hamburger-menu` element should show/hide the menu.
+var hamburgerMenu = document.getElementById('main-menu');
+var getBTN = document.getElementsByClassName("hamburger-btn")[0];
 
 document.getElementsByClassName("hamburger-btn")[0].addEventListener("click", function(){
+    console.log('click')
     
-    var getBTN = document.getElementsByClassName("hamburger-btn")[0];
+    //var getBTN = document.getElementsByClassName("hamburger-btn")[0];
+    //var hamburgerMenu = document.getElementById('main-menu');
     if(getBTN.ariaExpanded == "true")
     {
+        console.log('if')
+        hamburgerMenu.classList.remove('show-menu')
         getBTN.ariaExpanded = "false";
     }
     else
     {
-
+        console.log('else')
+        hamburgerMenu.classList.add('show-menu')
         getBTN.ariaExpanded = "true";
         getBTN.ariaHidden = "false"
     }
@@ -20,6 +27,23 @@ document.getElementsByClassName("hamburger-btn")[0].addEventListener("click", fu
     //     // alert(getMenu.getElementsByTagName("li")[i].style.visibility)
     // }
 })
+
+
+document.getElementsByTagName("body")[0].addEventListener("click", function(e){
+   
+    if (e.target.classList.contains("hamburger-btn")||(hamburgerMenu.contains(e.target))||(e.target.classList.contains('hamburger-menu'))){
+        console.log('ignore')
+    } else {
+        hamburgerMenu.classList.remove('show-menu')
+    }
+})
+function handleKey(e){
+    if (e.key == "Escape") {
+        hamburgerMenu.classList.remove('show-menu')
+        getBTN.focus()
+    }
+}
+document.onkeyup = handleKey
 //Clicking outside of `.hamburger-menu` should close the menu if it is open.
 //Clicking inside of `.hamburger-menu` should NOT close the menu.
 
